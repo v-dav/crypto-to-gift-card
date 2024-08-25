f4142482e85000fec7e393994e8515206ad9eddf
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-08-25 15:24:23 +0200

    change placeholder

--- src/components/USDInput.jsx
+++ src/components/USDInput.jsx
@@ -1,15 +1,13 @@
 function USDInput({ value, onChange }) {
 	return (
 		<div className="usd-input-container">
-			<label htmlFor="usd-amount">Enter Amount (USD)</label>
 			<div className="input-wrapper">
-				<span className="currency-symbol">$</span>
 				<input
 					type="number"
 					id="usd-amount"
 					value={value}
 					onChange={onChange}
-					placeholder="0.00"
+					placeholder="Enter amount (USD)"
 					min="0"
 					step="0.01"
 				/>
