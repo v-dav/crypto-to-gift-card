81ccabf54362465a401b2e18c938e0faf1236474
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-08-25 20:04:12 +0200

    prepare to deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Footer from './components/Footer';
